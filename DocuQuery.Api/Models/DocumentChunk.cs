5aea384a70bfee14cd3ffc9742444dc2d5640228
Ezana Tefera <etefera@pcatg.com>
2026-05-03 17:51:21 -0400

    added DocumentChunk vector store model.

---
+++ DocuQuery.Api/Models/DocumentChunk.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.VectorData;
+namespace DocuQuery.Api.Models
+{
+    public class DocumentChunk
+    {
+        [VectorStoreKey]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        [VectorStoreData]
+        public string FileName { get; set; } = string.Empty;
+        [VectorStoreData]
+        public int PageNumber { get; set; }
+        [VectorStoreData]
+        public string Content { get; set; } = string.Empty;
+        [VectorStoreVector(3072)]
+        public ReadOnlyMemory<float> Embedding { get; set; }
+    }
+}
