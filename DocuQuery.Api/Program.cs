5903f2f7f7bf186ca6674546a7445fff6289f2c6
Ezana Tefera <etefera@pcatg.com>
2026-05-04 23:40:41 -0400

    disabled swagger in production.

--- DocuQuery.Api/Program.cs
+++ DocuQuery.Api/Program.cs
@@ -58,14 +58,11 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseForwardedHeaders();
 app.UseCors("Angular");
